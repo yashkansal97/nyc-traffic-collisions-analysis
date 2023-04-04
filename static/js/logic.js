c610fc7b5a4bea016a7d27efb493e1762ff859cf
yeyanwang <yeyanwang@icloud.com>
2023-04-03 19:15:46 -0700

    update css, html, js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,4 @@
+
 // Create our map
 var myMap = L.map("map", {
     center: [40.730610, -73.935242],
@@ -9,31 +10,48 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href=https://www.openstreetmap.org/copyright>OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-
 // Fetch from SQL database
-d3.json('http://localhost:5000/data').then(function(data) {
+// d3.json('http://127.0.0.1:5000/data').then(function(data) {
+d3.json('/data').then(function(data) {
+    d3.json('/boroughs').then(function(boroughs){
     
-    console.log(data);
+        console.log(data);
+        console.log(boroughs);
 
+        // Create a layer for the boroughs polygons
+        function boroughPolygons(){
+            let features = boroughs['features'];
+            let geojsonLayer = L.geoJSON(features,{
+            'style': {'color': 'gold'}
+            })
+            geojsonLayer.addTo(myMap)
+        }
+        boroughPolygons()
 
-    // Create a new marker cluster group.
-    //var markers = L.markerClusterGroup();
+        // Create a new marker cluster group.
+        function createMarkers(){
+            let markerClusters = L.markerClusterGroup();
+            data.forEach((collision) => {
+                if (collision.latitude != null){
+                    var markers = L.marker([collision.latitude, collision.longitude], {
+                        draggable: false,
+                    });
 
-    // Loop through the data.
-    //data.forEach((response) => {
-        // Set the data location property to a variable.
-        //var location = response.location;
+                    markers.bindPopup(`No. of Persons Injuried: ${collision.number_of_persons_injuried} <br> 
+                                    No. of Persons Killed: ${collision.number_of_persons_killed} <br> 
+                                    Crash Date: ${moment(collision.crash_date).format("dddd, MMMM Do YYYY")} <br> 
+                                    Crash Time: ${collision.crash_time}`)
+                    markerClusters.addLayer(markers)
+                }
+            })
+            myMap.addLayer(markerClusters);
+        }
+        createMarkers()
 
-        // // Check for the location property.
-        // if (location) {
-        // // Add a new marker to the cluster group, and bind a popup.
-        // markers.addLayer(L.marker()
-        //     .bindPopup());
-        // }
+        // Create bar chart
+
 
     });
-
-    // Add our marker cluster layer to the map.
-    // myMap.addLayer(markers);
+});
 
 
